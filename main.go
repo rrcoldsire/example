7329b782893840362d876268faf4bb67cf5da346
rrcoldsire <ricky@coldsire.com>
2026-05-16 00:20:28 +0700

    add doc

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
+// Package main deofines the entry point of the Go application. It includes a function to greet the user by name.
 package main
 
 import "fmt"
 
+// Greeting takes a name as input and prints a greeting message to the console.
 func Greeting(name string) {
 	fmt.Printf("Hello, %s!\n", name)
 }
